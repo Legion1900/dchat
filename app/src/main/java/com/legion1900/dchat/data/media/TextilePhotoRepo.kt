0b2a20078778fdcfbcaab67a20ca3c3338344df7
Roman Chumachenko <chumachenkor@protonmail.com>
2020-11-09 15:01:21 +0200

    Implemented photo insertion in PhotoRepo.kt

--- app/src/main/java/com/legion1900/dchat/data/media/TextilePhotoRepo.kt
+++ app/src/main/java/com/legion1900/dchat/data/media/TextilePhotoRepo.kt
@@ -1,11 +1,13 @@
 package com.legion1900.dchat.data.media
 
 import com.legion1900.dchat.data.textile.abs.TextileProxy
+import com.legion1900.dchat.data.textile.blockHandler
 import com.legion1900.dchat.domain.media.PhotoRepo
 import com.legion1900.dchat.domain.media.PhotoWidth
 import io.reactivex.Single
 import io.reactivex.SingleEmitter
 import io.textile.textile.Handlers
+import java.io.File
 
 class TextilePhotoRepo(private val proxy: TextileProxy) : PhotoRepo {
     override fun getPhoto(photoId: String, width: PhotoWidth): Single<ByteArray> {
@@ -22,10 +24,21 @@ class TextilePhotoRepo(private val proxy: TextileProxy) : PhotoRepo {
             }
     }
 
+    override fun addPhoto(threadId: String, file: File): Single<String> {
+        return proxy.instance.flatMap { textile ->
+            Single.create { emitter ->
+                val handler =
+                    blockHandler({ emitter.onSuccess(it!!.data) }, { emitter.onError(it) })
+                textile.files.addFiles(file.absolutePath, threadId, null, handler)
+            }
+        }
+    }
+
     private fun getWidthValue(width: PhotoWidth): Long {
         return when (width) {
             PhotoWidth.SMALL -> 100
             PhotoWidth.MEDIUM -> 320
+            PhotoWidth.LARGE -> 800
         }
     }
 
--- app/src/main/java/com/legion1900/dchat/domain/media/PhotoRepo.kt
+++ app/src/main/java/com/legion1900/dchat/domain/media/PhotoRepo.kt
@@ -1,11 +1,13 @@
 package com.legion1900.dchat.domain.media
 
 import io.reactivex.Single
+import java.io.File
 
 interface PhotoRepo {
     fun getPhoto(photoId: String, width: PhotoWidth): Single<ByteArray>
+    fun addPhoto(threadId: String, file: File): Single<String>
 }
 
 enum class PhotoWidth {
-    SMALL, MEDIUM
+    SMALL, MEDIUM, LARGE
 }
