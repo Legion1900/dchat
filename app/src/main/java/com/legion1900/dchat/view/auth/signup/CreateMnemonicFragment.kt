ab6cf1cc4effb78c0ce216f83346508ff6a82939
Roman Chumachenko <chumachenkor@protonmail.com>
2020-11-13 13:25:03 +0200

    Added toolbar for CreateMnemonicFragment.kt

--- app/src/main/java/com/legion1900/dchat/view/auth/signup/CreateMnemonicFragment.kt
+++ app/src/main/java/com/legion1900/dchat/view/auth/signup/CreateMnemonicFragment.kt
@@ -5,14 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.legion1900.dchat.R
+import com.legion1900.dchat.databinding.FragmentCreateMnemonicBinding
+import com.legion1900.dchat.view.util.ToolbarUtil
 
 class CreateMnemonicFragment : Fragment() {
+
+    private var _binding: FragmentCreateMnemonicBinding? = null
+    private val binding get() = _binding!!
+
+    private val toolbarUtil = ToolbarUtil(this)
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_create_mnemonic, container, false)
+        _binding = FragmentCreateMnemonicBinding.inflate(inflater, container, false)
+        toolbarUtil.setupToolbar(binding.toolbar)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
