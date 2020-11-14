54993d54e8e2cbf855c4930695f80d5d14f4ebca
Roman Chumachenko <chumachenkor@protonmail.com>
2020-11-14 14:05:56 +0200

    Fixed broken title on screen rotation.

--- app/src/main/java/com/legion1900/dchat/view/util/ToolbarUtil.kt
+++ app/src/main/java/com/legion1900/dchat/view/util/ToolbarUtil.kt
@@ -1,15 +1,23 @@
 package com.legion1900.dchat.view.util
 
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class ToolbarUtil(private val fragment: Fragment) {
     fun setupToolbar(toolbar: Toolbar, config: AppBarConfiguration? = null) {
+        (fragment.requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
         val controller = fragment.findNavController()
         val barConfig = config ?: AppBarConfiguration(controller.graph)
-        toolbar.setupWithNavController(controller, barConfig)
+        /*
+        * Use AppCompatActivity.setupActionBarWithNavController if you need toolbar as action bar
+        * (e.g. for options menu);
+        * Otherwise use Toolbar.setupWithNavController
+        * */
+        (fragment.requireActivity() as AppCompatActivity)
+            .setupActionBarWithNavController(controller, barConfig)
     }
 }
