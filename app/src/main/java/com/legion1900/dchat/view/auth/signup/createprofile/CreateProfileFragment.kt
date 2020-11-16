fd373f5d790dde3478cf6cfd961ac566254d2e5d
Roman Chumachenko <chumachenkor@protonmail.com>
2020-11-16 21:19:28 +0200

    Implemented account registration.

--- app/src/main/java/com/legion1900/dchat/view/auth/signup/createprofile/CreateProfileFragment.kt
+++ app/src/main/java/com/legion1900/dchat/view/auth/signup/createprofile/CreateProfileFragment.kt
@@ -1,13 +1,17 @@
 package com.legion1900.dchat.view.auth.signup.createprofile
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.children
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.legion1900.dchat.R
 import com.legion1900.dchat.databinding.FragmentCreateProfileBinding
 import com.legion1900.dchat.view.main.ChatApplication
 import com.legion1900.dchat.view.main.di.FragmentContainer
@@ -32,7 +36,6 @@ class CreateProfileFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         inject()
-        Log.d("enigma", "ViewModel: $viewModel")
     }
 
     override fun onCreateView(
@@ -45,6 +48,24 @@ class CreateProfileFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.finishBtn.setOnClickListener(::onFinishClick)
+        viewModel.isCreated.observe(viewLifecycleOwner) {
+            navigateToChat()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+
+    override fun onStop() {
+        super.onStop()
+        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
@@ -53,5 +74,27 @@ class CreateProfileFragment : Fragment() {
     private fun inject() {
         container = ChatApplication.newFragmentContainer(CreateProfileViewModel::class.java)
         factory = container.resolve(ViewModelProvider.Factory::class)!!
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    private fun onFinishClick(v: View) {
+        val isValidName = viewModel.isNameValid(binding.userName.text.toString())
+        if (isValidName) {
+            showProgressBar()
+            viewModel.createAccount(args.mnemonic.asList())
+        } else {
+            binding.nameInput.error = "Enter proper name"
+        }
+    }
+
+    private fun showProgressBar() {
+        binding.apply {
+            root.children.forEach { it.isVisible = false }
+            progressBar.isVisible = true
+        }
+    }
+
+    private fun navigateToChat() {
+        findNavController().navigate(R.id.action_createProfile_to_chat)
     }
 }
--- app/src/main/java/com/legion1900/dchat/view/auth/signup/createprofile/CreateProfileViewModel.kt
+++ app/src/main/java/com/legion1900/dchat/view/auth/signup/createprofile/CreateProfileViewModel.kt
@@ -1,12 +1,32 @@
 package com.legion1900.dchat.view.auth.signup.createprofile
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.legion1900.dchat.domain.account.RegistrationManager
 import com.legion1900.dchat.domain.app.AppStateRepo
+import io.reactivex.disposables.CompositeDisposable
 
 class CreateProfileViewModel(
     private val registrationManager: RegistrationManager,
     private val appStateRepo: AppStateRepo
 ) : ViewModel() {
 
+    private val _isCreated = MutableLiveData<Boolean>()
+    val isCreated: LiveData<Boolean> = _isCreated
+
+    private val disposables = CompositeDisposable()
+
+    fun isNameValid(name: String): Boolean = name.isNotEmpty()
+
+    fun createAccount(mnemonic: List<String>) {
+        registrationManager.createAccount(mnemonic).subscribe {
+            appStateRepo.setLoggedIn(true)
+            _isCreated.postValue(true)
+        }.let(disposables::add)
+    }
+
+    override fun onCleared() {
+        disposables.dispose()
+    }
 }
