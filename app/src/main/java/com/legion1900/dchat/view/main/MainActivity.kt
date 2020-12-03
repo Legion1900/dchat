2e21b9461df66a94ff82ce8def6a22b714a17c55
Roman Chumachenko <chumachenkor@protonmail.com>
2020-12-03 14:17:36 +0200

    Removed unnecessary fields from MainActivity.kt.

--- app/src/main/java/com/legion1900/dchat/view/main/MainActivity.kt
+++ app/src/main/java/com/legion1900/dchat/view/main/MainActivity.kt
@@ -7,17 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import com.legion1900.dchat.R
-import com.legion1900.dchat.data.account.TextileMnemonicGenerator
-import com.legion1900.dchat.data.textile.abs.TextileProxy
-import com.legion1900.dchat.data.textile.abs.ThreadFileRepo
 import com.legion1900.dchat.databinding.ActivityMainBinding
-import com.legion1900.dchat.domain.account.ProfileManager
-import com.legion1900.dchat.domain.account.RegistrationManager
-import com.legion1900.dchat.domain.chat.AclManager
-import com.legion1900.dchat.domain.chat.ChatManager
-import com.legion1900.dchat.domain.chat.ChatRepo
-import com.legion1900.dchat.domain.contact.ContactManager
-import com.legion1900.dchat.domain.media.PhotoRepo
 import com.legion1900.dchat.view.main.di.activityContainer
 import com.legion1900.dchat.view.main.navigation.DirectionProvider
 import com.legion1900.dchat.view.main.navigation.FlowSelectorFragmentDirections
@@ -30,19 +20,6 @@ class MainActivity : AppCompatActivity() {
     private val viewModel by lazy {
         ViewModelProvider(this, factory)[MainViewModel::class.java]
     }
-
-    private lateinit var repoPath: String
-
-    private lateinit var proxy: TextileProxy
-    private lateinit var contactManager: ContactManager
-    private val mnemonicGenerator = TextileMnemonicGenerator()
-    private lateinit var registrationManager: RegistrationManager
-    private lateinit var profileManager: ProfileManager
-    private lateinit var photoRepo: PhotoRepo
-    private lateinit var chatRepo: ChatRepo
-    private lateinit var fileRepo: ThreadFileRepo
-    private lateinit var chatManager: ChatManager
-    private lateinit var aclManager: AclManager
 
     private lateinit var binding: ActivityMainBinding
 
